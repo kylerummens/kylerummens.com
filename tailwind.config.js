0c0f79ee6b8f44f4f6197297e08464051c1d3be6
Kyle Rummens <krummens@onetelgem.com>
2022-12-25 02:28:58 -0700

    Home page design

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,20 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      borderRadius: {
+        '4xl': '2rem'
+      },
+      colors: {
+        'github': '#24292f',
+        'linkedin': '#0073b1',
+        'twitter': '#1d9bf0',
+        'stackoverflow': '#f48225'
+      },
+      opacity: {
+        '15': '.15',
+      }
+    }
   },
   plugins: [
     require('@tailwindcss/typography')
