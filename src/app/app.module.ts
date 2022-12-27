95132233c1974b6c8885882d52a9631c0a3efa39
kylerummens <krummens@onetelgem.com>
2022-12-27 16:19:42 -0700

    Fixed social links grid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { TransferHttpCacheModule } from '@ngUniversal/common';
+import { TransferHttpCacheModule } from '@nguniversal/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
