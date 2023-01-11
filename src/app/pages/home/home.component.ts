9cb666224b237407cda6b0acfdd2973f8494bcb2
kylerummens <krummens@onetelgem.com>
2023-01-10 22:15:33 -0700

    Code links, site manifest, favicon

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BlogPost, BlogService } from 'src/app/services/blog.service';
 import { GithubContributions, GithubService } from 'src/app/services/github.service';
+import { SeoService } from 'src/app/services/seo.service';
 
 @Component({
   selector: 'app-home',
@@ -11,8 +12,31 @@ export class HomeComponent implements OnInit {
 
   blog_posts?: BlogPost[];
   contributions?: GithubContributions;
+  code_links: { link: string, title: string, description: string }[] = [
+    {
+      link: 'https://www.npmjs.com/package/kml-to-geojson',
+      title: 'kml-to-geojson',
+      description: 'Convert KML content to GeoJSON, preserving style and folder data'
+    },
+    {
+      link: 'https://github.com/kylerummens/angular-supabase-auth',
+      title: 'Angular Supabase Auth',
+      description: 'Full code example using Supabase Auth, Database, and Realtime to manage authentication in Angular'
+    },
+    {
+      link: 'https://github.com/kylerummens/kylerummens.com',
+      title: 'kylerummens.com',
+      description: 'Source code for my personal website, written in Typescript using Angular Universal'
+    },
+    {
+      link: 'https://github.com/kylerummens/kml-to-geojson',
+      title: 'kml-to-geojson',
+      description: 'Source code for the npm package kml-to-geojson, written in Typescript'
+    }
+  ]
 
   constructor(
+    private seoService: SeoService,
     private blogService: BlogService,
     private githubService: GithubService) { }
 
@@ -37,6 +61,22 @@ export class HomeComponent implements OnInit {
     }
   }
 
+  getCodeLinkImage(link: string) {
+    const base_url = this.getCodeLinkBaseUrl(link);
+
+    if (base_url === 'github.com') {
+      return '/assets/github.png';
+    }
+    else if (base_url === 'npmjs.com') {
+      return '/assets/npm.png';
+    }
+    return '';
+  }
+
+  getCodeLinkBaseUrl(link: string) {
+    return link.replace('https://', '').replace('http://', '').split('/')[0].replace('www.', '');
+  }
+
   private initBlogPosts() {
     this.blogService.getPosts().then(posts => {
       this.blog_posts = posts;
@@ -50,6 +90,11 @@ export class HomeComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.seoService.updateMetadata({
+      title: 'kylerummens.com',
+      description: `I'm a software engineer, rock climber, wannabe pilot, and recovering Dr.Pepper addict`,
+      image: '/assets/kylerummens.png'
+    });
     this.initGithubContributions();
     this.initBlogPosts();
   }
